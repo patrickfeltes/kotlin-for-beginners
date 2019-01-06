d4ac4bee8ee7a0888a28bd9f9f036bbf79dddecf
patrickfeltes <pj6444@gmail.com>
2019-01-05 22:42:22 -0600

    Code for tutorial 3

--- src/tutorial3/Tutorial3.kt
+++ src/tutorial3/Tutorial3.kt
@@ -1,5 +1,12 @@
 package tutorial3
 
-/**
- * Created by patrickfeltes on 1/5/19.
- */
+val byte: Byte = 1 // a byte stores positive and negative numbers in 8 bits
+val short: Short = 15 // same as above, but 16 bits
+val x: Int = 4 // same as above, but 32 bits
+val y: Long = 12321321L // same as above, but 64 bits
+
+val w: Float = 1.2F // a float stores positive and negative numbers with decimals in 32 bits
+val z: Double = 1.2 // same as above, but 64 bits
+
+val name: String = "Patrick" // a string is a list of characters, so it could be a word, name, sentence etc...
+val character: Char = '1' // a character stores a single character and nothing more!
